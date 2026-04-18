6bf571b6b823dc9c14255f8b42b288e8f35d761c
Bruno <andreobruno3@gmail.com>
2026-04-18 15:54:58 -0300

    Dashboar de docente para visualizar el formulario de alta

--- is1_2025_eti/target/classes/scheme.sql
+++ is1_2025_eti/target/classes/scheme.sql
@@ -1,9 +1,25 @@
--- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
-DROP TABLE IF EXISTS users;
-
 -- Crea la tabla 'users' con los campos originales, adaptados para SQLite
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+);
+
+DROP TABLE IF EXISTS persona;
+CREATE TABLE persona (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dni TEXT NOT NULL UNIQUE,
+    nombre TEXT NOT NULL,     
+    apellido TEXT NOT NULL,
+    direccion TEXT NOT NULL,
+    contacto TEXT NOT NULL UNIQUE      
+);
+
+DROP TABLE IF EXISTS docente;
+CREATE TABLE docente(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_person INTEGER NOT NULL UNIQUE,
+    matricula INTEGER NOT NULL UNIQUE,
+    CONSTRAINT fk_docente1 FOREIGN KEY (id_person) REFERENCES persona(id)
 );
